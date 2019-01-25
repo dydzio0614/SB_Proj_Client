620251624bd51abcd554a830a1e31c582d3259e0
Dydzio <blood990@gmail.com>
2019-01-25 19:45:48 +0100

    Add error 404 for wrong page

---
+++ src/components/error-pages/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>
+    ERROR 404 - RESOURCE NOT FOUND!
+  </p>
+</template>
+<script>
+export default {
+  name: 'NotFound'
+}
+</script>
+<style scoped>
+p {
+    font-weight: bold;
+    font-size: 50px;
+    text-align: center;
+    color: #f10b0b;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import NotFound from '@/components/error-pages/NotFound'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
