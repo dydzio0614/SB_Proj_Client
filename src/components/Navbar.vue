0f695d54819a8738442203fc18aa2fcca87a0c07
Dydzio <blood990@gmail.com>
2019-01-25 18:17:50 +0100

    Add bootstrap dependency, use it for navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-navbar
+    toggleable="md"
+    type="dark"
+    variant="info">
+    <b-navbar-brand href="#">Home</b-navbar-brand>
+    <b-navbar-nav>
+      <b-nav-item href="#">View Books</b-nav-item>
+      <b-nav-item href="#">Add Book</b-nav-item>
+      <b-nav-item href="#">Chat</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
